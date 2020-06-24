b7fa98897b78692fea88667dda230fa4c1f12620
Mykhailo Nester <nsmisha.dev@gmail.com>
2020-06-24 19:46:36 +0300

    - android methods and small update to UI;

--- android/src/main/java/com/reactlibrary/CrowdinModule.java
+++ android/src/main/java/com/reactlibrary/CrowdinModule.java
@@ -1,27 +1,102 @@
 package com.reactlibrary;
 
+import android.util.Log;
+
+import androidx.annotation.NonNull;
+
+import com.crowdin.platform.Crowdin;
+import com.crowdin.platform.CrowdinConfig;
+import com.crowdin.platform.LoadingStateListener;
+import com.crowdin.platform.util.ExtensionsKt;
+import com.facebook.react.bridge.Callback;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
-import com.facebook.react.bridge.Callback;
+
+import org.jetbrains.annotations.NotNull;
+
+import java.util.Locale;
 
 public class CrowdinModule extends ReactContextBaseJavaModule {
 
-    private final ReactApplicationContext reactContext;
+    private static ReactApplicationContext reactContext;
 
-    public CrowdinModule(ReactApplicationContext reactContext) {
-        super(reactContext);
-        this.reactContext = reactContext;
+    CrowdinModule(ReactApplicationContext context) {
+        super(context);
+        reactContext = context;
     }
 
+    @NonNull
     @Override
     public String getName() {
         return "Crowdin";
     }
 
     @ReactMethod
-    public void sampleMethod(String stringArgument, int numberArgument, Callback callback) {
-        // TODO: Implement some actually useful functionality
-        callback.invoke("Received numberArgument: " + numberArgument + " stringArgument: " + stringArgument);
+    public void initWithHashString(
+            @NonNull String distributionHash,
+            @NonNull String sourceLanguage,
+            @NotNull final Callback callback) {
+        Crowdin.init(reactContext, new CrowdinConfig.Builder()
+                .withDistributionHash(distributionHash)
+                .build());
+        final LoadingStateListener listener = new LoadingStateListener() {
+            @Override
+            public void onDataChanged() {
+                Crowdin.unregisterDataLoadingObserver(this);
+                callback.invoke("Localization downloaded");
+            }
+
+            @Override
+            public void onFailure(@NotNull Throwable throwable) {
+                Crowdin.unregisterDataLoadingObserver(this);
+                callback.invoke(throwable.getLocalizedMessage());
+            }
+        };
+
+        Crowdin.registerDataLoadingObserver(listener);
+    }
+
+    @ReactMethod(isBlockingSynchronousMethod = true)
+    public String getString(@NonNull String key) {
+        String formattedCode = ExtensionsKt.getFormattedCode(Locale.getDefault());
+        return Crowdin.getString(formattedCode, key);
+    }
+
+    @ReactMethod(isBlockingSynchronousMethod = true)
+    public String getStringByLocale(@NonNull String languageCode, @NonNull String key) {
+        if (languageCode.isEmpty()) {
+            languageCode = ExtensionsKt.getFormattedCode(Locale.getDefault());
+        }
+
+        return Crowdin.getString(languageCode, key);
+    }
+
+    @ReactMethod(isBlockingSynchronousMethod = true)
+    public String getResources() {
+        return Crowdin.getResources(Locale.getDefault().getLanguage());
+    }
+
+    @ReactMethod(isBlockingSynchronousMethod = true)
+    public void getResourcesByLocale(@NonNull String languageCode) {
+        Locale locale = getLocaleForLanguageCode(languageCode);
+        String formattedCode = ExtensionsKt.getFormattedCode(locale);
+        Log.d(CrowdinModule.class.getSimpleName(), languageCode);
+        Crowdin.getResources(formattedCode);
+    }
+
+    private Locale getLocaleForLanguageCode(String languageCode) {
+        String code = Locale.getDefault().getLanguage();
+        Locale locale;
+        try {
+            String[] localeData = languageCode.split("-");
+            code = localeData[0];
+            String region = localeData[1];
+            locale = new Locale(code, region);
+        } catch (Exception ex) {
+            locale = new Locale(code);
+        }
+
+        return locale;
     }
 }
--- android/src/main/java/com/reactlibrary/CrowdinPackage.java
+++ android/src/main/java/com/reactlibrary/CrowdinPackage.java
@@ -1,23 +1,31 @@
 package com.reactlibrary;
 
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
-
 import com.facebook.react.ReactPackage;
 import com.facebook.react.bridge.NativeModule;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.uimanager.ViewManager;
-import com.facebook.react.bridge.JavaScriptModule;
+
+import org.jetbrains.annotations.NotNull;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 public class CrowdinPackage implements ReactPackage {
-    @Override
-    public List<NativeModule> createNativeModules(ReactApplicationContext reactContext) {
-        return Arrays.<NativeModule>asList(new CrowdinModule(reactContext));
-    }
 
+    @NotNull
     @Override
-    public List<ViewManager> createViewManagers(ReactApplicationContext reactContext) {
+    public List<ViewManager> createViewManagers(@NotNull ReactApplicationContext reactContext) {
         return Collections.emptyList();
     }
+
+    @NotNull
+    @Override
+    public List<NativeModule> createNativeModules(@NotNull ReactApplicationContext reactContext) {
+        List<NativeModule> modules = new ArrayList<>();
+        modules.add(new CrowdinModule(reactContext));
+
+        return modules;
+    }
+
 }
